dd20d86d9d48eef69fc251b3b42f885c8caaf870
USER <sithiraroneth@gmail.com>
2024-03-13 10:47:02 +0530

    add user

--- src/main/java/lk/ijse/Controller/DashBordController.java
+++ src/main/java/lk/ijse/Controller/DashBordController.java
@@ -5,57 +5,128 @@
  * */
 package lk.ijse.Controller;
 
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class DashBordController {
-
+    @FXML
+    private Label lbldesc;
+    @FXML
+    private Label lblmenu;
     @FXML
     private AnchorPane root;
+    @FXML
+    private ImageView setting;
+    @FXML
+    private ImageView transaction;
+    @FXML
+    private ImageView user;
+    @FXML
+    private ImageView book;
 
     @FXML
-    void BookPage(MouseEvent event) throws IOException {
-        Parent rootNode = FXMLLoader.load(getClass().getResource("/View/book.fxml"));
-        Scene scene = new Scene(rootNode);
-        Stage primaryStage = (Stage) root.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        primaryStage.setTitle("Book");
+    void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            switch (icon.getId()) {
+                case "user":
+                    lblmenu.setText("Manage Users");
+                    lbldesc.setText("Click to add, edit, delete, search or view Users");
+                    break;
+                case "book":
+                    lblmenu.setText("Manage  Books");
+                    lbldesc.setText("Click to add, edit, delete, search or view Books");
+                    break;
+                case "transaction":
+                    lblmenu.setText("Transaction");
+                    lbldesc.setText("Click here if you want to Transaction");
+                    break;
+                case "setting":
+                    lblmenu.setText("Setting");
+                    lbldesc.setText("Click if you want to change password");
+                    break;
+            }
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
     }
 
     @FXML
-    void CustomerPage(MouseEvent event) throws IOException {
-        Parent rootNode = FXMLLoader.load(getClass().getResource("/View/user.fxml"));
-        Scene scene = new Scene(rootNode);
-        Stage primaryStage = (Stage) root.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        primaryStage.setTitle("Customer");
+    void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            icon.setEffect(null);
+            lblmenu.setText("Welcome");
+            lbldesc.setText("Please select one of above main operations to proceed");
+        }
+    }
+    @FXML
+    void navigate(MouseEvent event) throws IOException {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            Parent root = null;
+
+            switch (icon.getId()) {
+                case "user":
+                    root = FXMLLoader.load(this.getClass().getResource("/View/user.fxml"));
+                    break;
+                case "book":
+                    root = FXMLLoader.load(this.getClass().getResource("/View/book.fxml"));
+                    break;
+                case "transaction":
+                    root = FXMLLoader.load(this.getClass().getResource("/View/transaction.fxml"));
+                    break;
+                case "setting":
+                    root = FXMLLoader.load(this.getClass().getResource("/View/setting.fxml"));
+                    break;
+            }
+
+            if (root != null) {
+                Scene subScene = new Scene(root);
+                Stage primaryStage = (Stage) this.root.getScene().getWindow();
+                primaryStage.setScene(subScene);
+                primaryStage.centerOnScreen();
+
+                TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+                tt.setFromX(-subScene.getWidth());
+                tt.setToX(0);
+                tt.play();
+
+            }
+        }
     }
 
-    @FXML
-    void TransactionPage(MouseEvent mouseEvent) throws IOException {
-        Parent rootNode = FXMLLoader.load(getClass().getResource("/View/transaction.fxml"));
-        Scene scene = new Scene(rootNode);
-        Stage primaryStage = (Stage) root.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        primaryStage.setTitle("Transaction");
-    }
-    @FXML
-    void SettingPage(MouseEvent event) throws IOException {
-        Parent rootNode = FXMLLoader.load(getClass().getResource("/View/setting.fxml"));
-        Scene scene = new Scene(rootNode);
-        Stage primaryStage = (Stage) root.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        primaryStage.setTitle("Setting");
-    }
+
 }
---
+++ src/main/java/lk/ijse/Controller/User_Book_Controller.java
@@ -0,0 +1,33 @@
+/* Created By Sithira Roneth
+ * Date :3/13/24
+ * Time :10:35
+ * Project Name :ORM
+ * */
+package lk.ijse.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class User_Book_Controller {
+    @FXML
+    private ImageView home;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    public void navigate(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/user-dash.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+    }
+}
