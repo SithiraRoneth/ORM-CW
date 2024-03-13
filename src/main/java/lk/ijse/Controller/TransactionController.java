f4bd47cb1cab0747c2be5f5932b588e8526b87a3
USER <sithiraroneth@gmail.com>
2024-03-13 11:07:28 +0530

    edit transaction.fxml

--- src/main/java/lk/ijse/Controller/TransactionController.java
+++ src/main/java/lk/ijse/Controller/TransactionController.java
@@ -5,10 +5,17 @@
  * */
 package lk.ijse.Controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -16,15 +23,60 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class TransactionController {
+
+    @FXML
+    private JFXComboBox<String> cmbBookId;
+
+    @FXML
+    private JFXComboBox<String> cmbUserID;
+
+    @FXML
+    private TableColumn<?, ?> colBook;
+
+    @FXML
+    private TableColumn<?, ?> colDelete;
+
+    @FXML
+    private TableColumn<?, ?> colReturn;
+
+    @FXML
+    private TableColumn<?, ?> colStart;
+
+    @FXML
+    private TableColumn<?, ?> colUser;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblid;
+
+    @FXML
+    private DatePicker returnDate;
+
     @FXML
     private AnchorPane rootNode;
 
     @FXML
-    void HomePage(MouseEvent mouseEvent) throws IOException {
+    private TableView<?> tbl;
+
+    @FXML
+    private TextField txtbookName;
+
+    @FXML
+    private TextField txtusername;
+
+    @FXML
+    void HomePage(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/dash-board.fxml"));
         Scene scene = new Scene(root);
         Stage primaryStage = (Stage) rootNode.getScene().getWindow();
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
+    }
+
+    @FXML
+    void saveOnAction(ActionEvent event) {
+
     }
 }
